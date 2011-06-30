4493b05b8d2257bcc6b8e7dfe4c9d6388995c671
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Add a user method

--- chronos-netty/src/main/java/com/chronos/netty/ChronosDispatcher.java
+++ chronos-netty/src/main/java/com/chronos/netty/ChronosDispatcher.java
@@ -2,16 +2,13 @@ package com.chronos.netty;
 
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST;
 import static org.jboss.netty.handler.codec.http.HttpResponseStatus.CREATED;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.NOT_FOUND;
 import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 import java.util.Map;
 
-import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpResponse;
-import org.jboss.netty.util.CharsetUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.chronos.biz.App;
 import com.chronos.biz.QuizzApi;
@@ -21,27 +18,36 @@ import com.chronos.biz.QuizzApi;
  * @version $Id$
  */
 public class ChronosDispatcher {
-    final Logger logger = LoggerFactory.getLogger(ChronosDispatcher.class);
     QuizzApi quizzApi = App.get();
-    final String messageBadRequest = "Unknow service";
 
     public ServiceResponse dispatcher(ServiceResquest serviceResquest) throws Exception {
         // TODO : reflection scale ?
-
+        // TODO : send to a delegate
         if ("user".equals(serviceResquest.method)) {
+            return user(serviceResquest);
+        }
+
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, NOT_FOUND);
+        ServiceResponse serviceResponse = new ServiceResponse();
+        serviceResponse.httpResponse = response;
+        return serviceResponse;
+    }
+
+    private ServiceResponse user(ServiceResquest serviceResquest) {
+        try {
+            // TODO : throw exception costs !
             Map<String, String> args = serviceResquest.args;
             quizzApi.addUser(args.get("lastname"), args.get("firstname"), args.get("mail"), args.get("password"));
             HttpResponse response = new DefaultHttpResponse(HTTP_1_1, CREATED);
             ServiceResponse serviceResponse = new ServiceResponse();
             serviceResponse.httpResponse = response;
             return serviceResponse;
+        } catch (Exception e) {
+            HttpResponse response = new DefaultHttpResponse(HTTP_1_1, BAD_REQUEST);
+            ServiceResponse serviceResponse = new ServiceResponse();
+            serviceResponse.httpResponse = response;
+            return serviceResponse;
         }
-
-        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, BAD_REQUEST);
-        response.setContent(ChannelBuffers.copiedBuffer(messageBadRequest, CharsetUtil.UTF_8));
-        ServiceResponse serviceResponse = new ServiceResponse();
-        serviceResponse.httpResponse = response;
-        return serviceResponse;
     }
 
 }
