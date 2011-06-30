899213606f1c912a0ab44e174645541171be6a95
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Improving setup

--- chronos-couch/setup.sh
+++ chronos-couch/setup.sh
@@ -5,8 +5,12 @@ set -x
 CHRONOS_HOST=$1
 CHRONOS_PORT=$2
 
+# https://github.com/zpoley/json-command
+# npm install json
+
 JSON="`cat ./sample/gamesession-sample-encode.json`"
-curl -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/game
+RETURNVALUE=`curl -silent -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/game --write-out %{http_code}`
+assert(201, $RETURNVALUE);
 
 curl -iX POST -H Accept:application/json -H Content-Type:application/json -d '{"firstname" : "olivier", "lastname" : "bazoud", "mail" : "null@gmail.com","password" : "secret"}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/user
 
@@ -15,3 +19,12 @@ curl -iX POST -H Accept:application/json -H Content-Type:application/json -d '{"
 curl -iX GET -H session_key:XXX http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/question/1
 
 curl -iX POST -H session_key:XXX -H Accept:application/json -H Content-Type:application/json -d '{"answer":1}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/answer/1
+
+
+assert() {
+  if [ "${1}" -ne "${1}" ]; then
+        echo "Expecting $1 but was $2"
+        exit 1
+  fi
+}
+
