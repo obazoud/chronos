0a5245db203492c7ddc6d9121d12cf58f0cab90d
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    User superadmin

--- chronos-couch/createdb.sh
+++ chronos-couch/createdb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-curl -X PUT http://localhost:5984/thechallenge
+curl -X PUT http://localhost:5984/_config/admins/superadmin -d '"supersecret"'
 
-curl -X PUT http://localhost:5984/thechallenge/_design/warmup -d @design/warmup.json
+curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge
 
-curl -X PUT http://localhost:5984/thechallenge/game1 -d '{"gamers":0}'
+curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/warmup -d @design/warmup.json
 
-curl -X PUT http://127.0.0.1:5984/_config/admins/superadmin -d '"supersecret"'
+curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/game1 -d '{"gamers":0}'
+
