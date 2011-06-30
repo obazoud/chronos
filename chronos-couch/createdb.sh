4ee72c44663a44ffa7129dc7e945c0b921399925
Slim Tebourbi <slimtbourbi@gmail.com>
2011-06-30 22:00:00 +0200

    maj de la fonction de validation pour prise en compte de la phase de warmup

--- chronos-couch/createdb.sh
+++ chronos-couch/createdb.sh
@@ -1,17 +1,30 @@
-#!/bin/bash
+#!/bin/bash 
+# -e
+ADMIN_NAME=$1
+ADMIN_PASS=$2
+DB_HOST_NAME=$3
+DB_PORT=$4
 
-curl -X PUT http://localhost:5984/_config/admins/superadmin -d '"supersecret"'
+HOST=$DB_HOST_NAME:$DB_PORT
 
+
+#curl -X PUT http://$HOST/_config/admins/$ADMIN_NAME -d '"${ADMIN_PASS}"'
+
+HOST_ADMIN=$ADMIN_NAME:$ADMIN_PASS@$HOST
+
+
+DB_URL=http://$HOST_ADMIN/thechallenge
 # Delete and recreate database
-curl -X DELETE http://superadmin:supersecret@localhost:5984/thechallenge
-curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge
+curl -X DELETE $DB_URL
+curl -X PUT $DB_URL
 
-curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/warmup -d @design/warmup.json
+curl -X PUT $DB_URL/_design/warmup -d @design/warmup.json
 
-curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/game1 -d '{"gamers":0}'
+curl -X PUT $DB_URL/game1 -d '{"gamers":0,"type":"warmup"}'
 
-curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/validate -d @design/validate.json
+curl -X PUT $DB_URL/_design/validate -d @design/validate.json
 
-JSON="`cat ./sample/gamesession-sample-encode.json`"
-curl -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://localhost:8080/api/game
+# ne mettre ici que ce qui est specifique a couch et peut fonctionner en se limitant a une instance couch
+#JSON="`cat ./sample/gamesession-sample-encode.json`"
+#curl -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://localhost:8080/api/game
 
