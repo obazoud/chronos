555dc924d73cb1dd732a3dd8211362eb04b9e707
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Netty: comment HttpChunkAggregator

--- chronos-netty/src/main/java/com/chronos/netty/pipeline/HttpServerPipelineFactory.java
+++ chronos-netty/src/main/java/com/chronos/netty/pipeline/HttpServerPipelineFactory.java
@@ -4,7 +4,6 @@ import static org.jboss.netty.channel.Channels.pipeline;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -20,7 +19,7 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
     public ChannelPipeline getPipeline() throws Exception {
         ChannelPipeline pipeline = pipeline();
         pipeline.addLast("httpDecoder", new HttpRequestDecoder());
-        pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
+        // pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
         pipeline.addLast("httpEncoder", new HttpResponseEncoder());
         pipeline.addLast("chronos", new ChronosHandler());
         return pipeline;
