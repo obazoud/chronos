d63b2fcb9843209c41fc29148af6b7d0b32d1ca8
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Use aes instead of base64

--- chronos-node/security.js
+++ chronos-node/security.js
@@ -1,4 +1,5 @@
-var nibbler = require('./externals/nibbler');
+var crypto = require('crypto');
+var key = '1hv4pmT';
 
 Unauthorized = function (msg) {
     this.status = 401;
@@ -10,7 +11,7 @@ exports.authorize = function(req, body, cb) {
   if (!req.headers['cookie']) {
     return cb(new Unauthorized());
   }
-  var session_key = nibbler.b64decode(req.headers['cookie'].split('=')[1]);
+  var session_key = exports.decrypt(req.headers['cookie'].split('=')[1]);
   var sessionkeyjson;
   try {
     sessionkeyjson = JSON.parse(session_key);
@@ -22,5 +23,29 @@ exports.authorize = function(req, body, cb) {
 };
 
 exports.encode = function(data) {
-  return nibbler.b64encode(JSON.stringify(data));
+  return exports.crypt(JSON.stringify(data));
 };
+
+
+exports.crypt = function(text) {
+  var cipher = crypto.createCipher('aes-128-cbc', key);
+  var crypted = cipher.update(text,'utf8','hex');
+  crypted += cipher.final('hex');
+  return crypted;
+};
+
+exports.decrypt = function(text) {
+  var decipher = crypto.createDecipher('aes-128-cbc', key);
+  var dec = decipher.update(text,'hex','utf8');
+  dec += decipher.final('utf8');
+  return dec;
+};
+
+/*
+var text = "{'test':'crypto'}";
+var crypted = exports.crypt(text);
+console.log(crypted);
+var decypted = exports.decrypt(crypted);
+console.log(decypted);
+*/
+
