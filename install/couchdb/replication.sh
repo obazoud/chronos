42f16835baa4bd9cf575a7ab659305ca1d6850d4
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Fix couchdb replication

--- install/couchdb/replication.sh
+++ install/couchdb/replication.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Run in crontab
-curl -iX POST -d '{“source”:”thechallenge”,”target”:”http://192.168.1.1:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
-curl -iX POST -d '{“source”:”thechallenge”,”target”:”http://192.168.1.2:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
-curl -iX POST -d '{“source”:”thechallenge”,”target”:”http://192.168.1.4:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
-curl -iX POST -d '{“source”:”thechallenge”,”target”:”http://192.168.1.201:5984/thechallenge”}' http://192.168.1.3:5984/_replicate
+curl -iX POST -H "Content-Type:application/json" -d '{“source”:”thechallenge”,”target”:”http://192.168.1.1:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
+curl -iX POST -H "Content-Type:application/json" -d '{“source”:”thechallenge”,”target”:”http://192.168.1.2:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
+curl -iX POST -H "Content-Type:application/json" -d '{“source”:”thechallenge”,”target”:”http://192.168.1.4:5984/thechallenge”}'   http://192.168.1.3:5984/_replicate
+curl -iX POST -H "Content-Type:application/json" -d '{“source”:”thechallenge”,”target”:”http://192.168.1.201:5984/thechallenge”}' http://192.168.1.3:5984/_replicate
 
