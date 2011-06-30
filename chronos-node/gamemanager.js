ed55027fea99bcf89d9127c63bae4241a87611a1
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Cleaning

--- chronos-node/gamemanager.js
+++ chronos-node/gamemanager.js
@@ -136,20 +136,16 @@ subscriber.on("unsubscribe", function (channel, count) {
 });
 
 subscriber.on('message', function(channel, message) {
-  // logger.log(channel + ': ' + message);
   var json = JSON.parse(message);
   switch (json.event) {
     case 'initGame':
         gameState.initGame(json.message);
-        // logger.log(sys.inspect(gameState, false));
       break;
     case 'warmupStarts':
         gameState.warmupStarts(json.warmupStartDate);
-        // logger.log(sys.inspect(gameState, false));
       break;
     case 'warmupEnds':
         gameState.warmupEnds(json.warmupEnd);
-        // logger.log(sys.inspect(gameState, false));
       break;
     default:
       logger.log('Unknow event:' + json.event);
