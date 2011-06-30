c28d62e0f41e08a1b1b8a3844d4f1ad88a3db36f
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Add logger handler

--- chronos-netty/src/main/java/com/chronos/netty/HttpServerPipelineFactory.java
+++ chronos-netty/src/main/java/com/chronos/netty/HttpServerPipelineFactory.java
@@ -2,25 +2,35 @@ package com.chronos.netty;
 
 import static org.jboss.netty.channel.Channels.pipeline;
 
+import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
-import org.jboss.netty.handler.stream.ChunkedWriteHandler;
 
 /**
  * @author bazoud
  * @version $Id$
  */
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
+    private static ChannelHandler LOGGER;
+    private static final ChannelHandler DECODER = new HttpRequestDecoder();
+    private static final ChannelHandler ENCODER = new HttpResponseEncoder();
+    // private static final ChannelHandler CHUNKDED_WRITER = new ChunkedWriteHandler();
+    private static final ChannelHandler CHRONOS = new ChronosHandler();
+
+    public HttpServerPipelineFactory() {
+//        InternalLoggerFactory.setDefaultFactory(new Slf4JLoggerFactory());
+//        LOGGER = new LoggingHandler(InternalLogLevel.DEBUG);
+    }
 
     @Override
     public ChannelPipeline getPipeline() throws Exception {
         ChannelPipeline pipeline = pipeline();
-        pipeline.addLast("decoder", new HttpRequestDecoder());
-        pipeline.addLast("encoder", new HttpResponseEncoder());
-        pipeline.addLast("chunkedWriter", new ChunkedWriteHandler());
-        pipeline.addLast("handler", new ChronosHandler());
+//        pipeline.addLast("logger", LOGGER);
+        pipeline.addLast("decoder", DECODER);
+        pipeline.addLast("encoder", ENCODER);
+        pipeline.addLast("handler", CHRONOS);
         return pipeline;
     }
 
