ba88b5d894737569b9604f967e4cf99a0e46c3a9
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    api/game: add sample game to set up database

--- chronos-couch/createdb.sh
+++ chronos-couch/createdb.sh
@@ -2,6 +2,8 @@
 
 curl -X PUT http://localhost:5984/_config/admins/superadmin -d '"supersecret"'
 
+# Delete and recreate database
+curl -X DELETE http://superadmin:supersecret@localhost:5984/thechallenge
 curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge
 
 curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/warmup -d @design/warmup.json
@@ -10,4 +12,6 @@ curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/game1 -d '
 
 curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/validate -d @design/validate.json
 
-#curl -X PUT http://superadmin:supersecret@localhost:5984/thechallenge/_design/game -d @design/game.json
+JSON="`cat ./sample/gamesession-sample.json`"
+curl -X POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://localhost:8080/api/game
+
