64a902b4a58e4b4bd647423267c340208fa1ec22
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    add log

--- chronos-node/gamemanager.js
+++ chronos-node/gamemanager.js
@@ -133,6 +133,7 @@ function GameState() {
       // TODO really need ?
       for (i = 2; i <= numberOfQuestions + 1; i++) {
         this.sessions[i] = this.sessions[i - 1] + this.questiontimeframe + this.synchrotime;
+        logger.log('sessions' + i + ': ' + this.sessions[i]);
       }
     } else {
       logger.log('Already in state 3');
