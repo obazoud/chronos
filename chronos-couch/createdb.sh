8c77ab468fa4192ff6382f6919293d8dc2851bdb
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    api/getAnswer: add score

--- chronos-couch/createdb.sh
+++ chronos-couch/createdb.sh
@@ -24,6 +24,8 @@ curl -X PUT $DB_URL/game1 -d '{"gamers":0,"type":"warmup"}'
 
 curl -X PUT $DB_URL/_design/validate -d @design/validate.json
 
+curl -X PUT $DB_URL/_design/answer -d @design/answer.json
+
 # ne mettre ici que ce qui est specifique a couch et peut fonctionner en se limitant a une instance couch
 #JSON="`cat ./sample/gamesession-sample-encode.json`"
 #curl -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://localhost:8080/api/game
