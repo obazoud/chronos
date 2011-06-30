56cb23c1390c0e17dc8ca00c1cd7583aafc4e8dc
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Remove Couchdb game cache

--- chronos-node/chronos-couchdb-api.js
+++ chronos-node/chronos-couchdb-api.js
@@ -24,14 +24,6 @@ var couchdbaseburl = 'http://' + host + ':' + port;
 var couchdburl = couchdbaseburl + '/thechallenge';
 var saltvalue = '1';
 
-// document in cache is supposed to be read only!!
-// otherwise clone it
-var store = {};
-var keys = {
-    'game': true
-};
-var timerId;
-
 exports.config = function(options) {
   options.success({host:host, port:port});
 };
@@ -74,25 +66,8 @@ exports.putDoc = function(name, batch, json, options) {
     }
   })
   .on('complete', function(data, response) {
-    if (keys[name]) {
-      restler.get(couchdburl + '/' + name, {
-        data: ''
-      })
-      .on('error', function(alldata, response2) {
-        if (options && options.error) {
-          options.error(data);
-        }
-      })
-      .on('complete', function(alldata, response2) {
-        store[name] = alldata;
-        if (options && options.success) {
-          options.success(data);
-        }
-      });
-    } else {
-      if (options && options.success) {
-        options.success(data);
-      }
+    if (options && options.success) {
+      options.success(data);
     }
   });
 };
@@ -114,12 +89,6 @@ exports.putDesign = function(name, options) {
 };
 
 exports.getDoc = function(name, options) {
-  if (store[name]) {
-    if (options && options.success) {
-      options.success(store[name]);
-      return;
-    }
-  }
   restler.get(couchdburl + '/' + name, {
     data: ''
   })
@@ -129,9 +98,6 @@ exports.getDoc = function(name, options) {
     }
   })
   .on('complete', function(data, response) {
-    if (keys[name]) {
-      store[name] = data;
-    }
     if (options && options.success) {
       options.success(data);
     }
@@ -179,16 +145,6 @@ exports.delete = function(name, id, options) {
 
 };
 
-exports.purge = function(name) {
-  // console.log(sys.inspect(store));
-  if (name) {
-    delete(store[name]);
-  } else {
-    store = {};
-  }
-  // console.log(sys.inspect(store));
-};
-
 exports.info = function(options) {
   restler.request(couchdburl, {
     data: '',
@@ -209,114 +165,4 @@ exports.info = function(options) {
     }
   });
 };
-
-changesStream = function(query, options) {
-  var stream = new process.EventEmitter();
-  var client = http.createClient(port, host, false);
-  var path = '/thechallenge/_changes?' + querystring.stringify(query);
-  console.log('Couchdb changes url : ' + couchdbaseburl + '/' + path);
-  var request = client.request('GET', path);
-  var buffer = '';
-
-  client.setTimeout(options.timeout);
-  request.addListener("response", function(res) {
-    res.addListener('data', function(chunk) {
-      //console.log('...');
-      buffer += (chunk || '');
-
-      var offset, change;
-      while ((offset = buffer.indexOf("\n")) >= 0) {
-        change = buffer.substr(0, offset);
-        buffer = buffer.substr(offset +1);
-
-        // Couch sends an empty line as the "heartbeat"
-        if (change == '') {
-          return stream.emit('heartbeat');
-        }
-
-        try {
-          change = JSON.parse(change);
-        } catch (e) {
-          return stream.emit('error', 'invalid json: '+change);
-        }
-
-        stream.emit('data', change);
-      }
-    })
-  });
-  request.end();
-
-  client.addListener('close', function(hadError) {
-    stream.emit('end', hadError);
-  });
-
-  stream.close = function() {
-    return client.destroy();
-  };
-
-  return stream;
-};
-
-couchdbChangesStream = function(filter, docname, callback, endCallback) {
-  var stream;
-  var since;
-
-  exports.info({
-    error: function(data) {
-      endCallback();
-    },
-    success: function(data) {
-      couchdbAccessFailed = false;
-      clearTimeout(timerId);
-      timerId = null;
-      var db = JSON.parse(data);
-      console.log('Couchdb update_seq: ' + db.update_seq);
-      console.log('Couchdb instance_start_time: ' + db.instance_start_time);
-      stream = changesStream({
-          feed: "continuous",
-          filter: filter,
-          name: docname,
-          heartbeat: 30 * 1000,
-          since: db.update_seq
-        }, {
-          timeout: 1
-        }
-      );
-      sys.inspect(stream);
-      stream.addListener('data', callback);
-      stream.addListener('end', endCallback);
-      return stream;
-    }
-  });
-};
-
-couchdbChangesStreamCallback = function(change) {
-  // console.log('couchdbChangesStream: ' + sys.inspect(change));
-  console.log('Purging cache... ' + change.id);
-  if (change.id) {
-    exports.purge(change.id);
-  }
-};
-
-couchdbChangesStreamCallbackError = function(change) {
-  console.log('Can not access to couchdb.');
-  if (couchdbAccessFailed == false) {
-    emitter.emit('couchdbAccessFailed');
-  }
-};
-
-couchdbChangesStream('gameonly/game', 'game', couchdbChangesStreamCallback, couchdbChangesStreamCallbackError);
-
-emitter.on("couchdbAccessFailed",function() {
-  couchdbAccessFailed = true;
-  if (timerId == null) {
-    console.log('Creating an setInterval.');
-    timerId = setInterval(function() {
-      if (couchdbAccessFailed == true) {
-        console.log('Try again via couchdbChangesStream.');
-        couchdbChangesStream('gameonly/game', 'game', couchdbChangesStreamCallback, couchdbChangesStreamCallbackError);
-      }
-      }, 10000);
-  }
-});
 
