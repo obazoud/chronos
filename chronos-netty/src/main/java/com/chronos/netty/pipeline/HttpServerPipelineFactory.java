507b35bd5cc80230de2c249d5d69abf5084e3409
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Netty: do not share instance

--- chronos-netty/src/main/java/com/chronos/netty/pipeline/HttpServerPipelineFactory.java
+++ chronos-netty/src/main/java/com/chronos/netty/pipeline/HttpServerPipelineFactory.java
@@ -1,32 +1,28 @@
 package com.chronos.netty.pipeline;
 
-import org.jboss.netty.channel.ChannelHandler;
+import static org.jboss.netty.channel.Channels.pipeline;
+
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
 import com.chronos.netty.service.ChronosHandler;
-
-import static org.jboss.netty.channel.Channels.pipeline;
 
 /**
  * @author bazoud
  * @version $Id$
  */
 public class HttpServerPipelineFactory implements ChannelPipelineFactory {
-    private static final ChannelHandler HTTP_DECODER = new HttpRequestDecoder();
-    private static final ChannelHandler HTTP_ENCODER = new HttpResponseEncoder();
-    // private static final ChannelHandler HTTP_CHUNKDED_WRITER = new ChunkedWriteHandler();
-    // private static final ChannelHandler SECURITY = new SecurityHandler();
-    private static final ChannelHandler CHRONOS = new ChronosHandler();
 
     @Override
     public ChannelPipeline getPipeline() throws Exception {
         ChannelPipeline pipeline = pipeline();
-        pipeline.addLast("httpDecoder", HTTP_DECODER);
-        pipeline.addLast("httpEncoder", HTTP_ENCODER);
-        pipeline.addLast("chronos", CHRONOS);
+        pipeline.addLast("httpDecoder", new HttpRequestDecoder());
+        pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
+        pipeline.addLast("httpEncoder", new HttpResponseEncoder());
+        pipeline.addLast("chronos", new ChronosHandler());
         return pipeline;
     }
 
