1236ae1ac0d52aa470d78f9083fa382d3f1156b2
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Improve setup

--- chronos-couch/setup.sh
+++ chronos-couch/setup.sh
@@ -1,30 +1,61 @@
 #!/bin/bash 
-# -e
-set -x
-
-CHRONOS_HOST=$1
-CHRONOS_PORT=$2
-
+set -e
+#set -x
 # https://github.com/zpoley/json-command
 # npm install json
 
-JSON="`cat ./sample/gamesession-sample-encode.json`"
-RETURNVALUE=`curl -silent -iX POST -H Accept:application/json -H Content-Type:application/json -d "${JSON}" http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/game --write-out %{http_code}`
-assert(201, $RETURNVALUE);
+./createdb.sh superadmin supersecret localhost 5984
 
-curl -iX POST -H Accept:application/json -H Content-Type:application/json -d '{"firstname" : "olivier", "lastname" : "bazoud", "mail" : "null@gmail.com","password" : "secret"}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/user
+CHRONOS_HOST=localhost
+CHRONOS_PORT=8080
 
-curl -iX POST -H Accept:application/json -H Content-Type:application/json -d '{"mail" : "null@gmail.com", "password" : "secret"}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/login
-
-curl -iX GET -H session_key:XXX http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/question/1
-
-curl -iX POST -H session_key:XXX -H Accept:application/json -H Content-Type:application/json -d '{"answer":1}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/answer/1
-
-
-assert() {
-  if [ "${1}" -ne "${1}" ]; then
-        echo "Expecting $1 but was $2"
+assertEquals() {
+  if [ -f "$4" ]; then
+    echo "$1, response: `cat $4`"
+  fi
+  if [ "${2}" != "${3}" ]; then
+        echo "$1: expecting $2 but was $3"
         exit 1
   fi
 }
+
+assertNotNull() {
+  if [ "${2}" == "" ]; then
+        echo "$1 is null"
+        exit 1
+  fi
+}
+
+USER_REV=`curl --silent -X GET http://superadmin:supersecret@localhost:5984/_users/org.couchdb.user:null@gmail.com | json -C _rev`
+if [ "$USER_REV" != "" ]; then
+  curl --silent -X DELETE http://superadmin:supersecret@localhost:5984/_users/org.couchdb.user:null@gmail.com?rev=$USER_REV
+fi
+
+GAME_JSON="`cat ./sample/gamesession-sample-encode.json`"
+curl -iX POST -H "Accept:application/json" -H "Content-Type:application/json" -d "${GAME_JSON}" http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/game 2>&1 | awk '{ if ($1 == "HTTP/1.1") { httpcode=$2 } } END { print httpcode}' | while read httpcode; do 
+  assertEquals "Creating game, http code" 201 $httpcode
+done
+
+curl -iX POST -H Accept:application/json -H Content-Type:application/json -d '{"firstname" : "olivier", "lastname" : "bazoud", "mail" : "null@gmail.com","password" : "secret"}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/user 2>&1 | awk '{ if ($1 == "HTTP/1.1") { httpcode=$2 } } END { print httpcode }' | while read httpcode; do
+  assertEquals "Creating user, http code" 201 $httpcode
+done
+
+curl -iX POST -H "Accept:application/json" -H "Content-Type:application/json" -d '{"mail" : "null@gmail.com", "password" : "secret"}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/login 2>&1 | awk '{ if ($1 == "HTTP/1.1") { httpcode=$2 } if ($1 == "session_key:") { sessionkey=$2 } if ($1 == "session_key:") { data=$2 } } END { print httpcode, sessionkey }' | while read httpcode sessionkey; do
+  sessionkey=`echo ${sessionkey} | sed 's/\r//g'`
+  echo $data
+  assertEquals "Creating user, http code" 201 $httpcode
+  assertNotNull "Creating user, sessionkey" ${sessionkey}
+  
+  curl -iX GET -H "session_key:${sessionkey}" http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/question/1  2>&1 | awk '{ if ($1 == "HTTP/1.1") { print $2 } } END { print }' | sed ':a;N;$!ba;s/\n/ /g' | while read httpcode data; do
+    assertEquals "Question, http code" 200 $httpcode
+    assertEquals "Score 12345" 12345 $(echo "$data" | json -C score)
+
+    curl -iX POST -H "session_key:${sessionkey}" -H "Accept:application/json" -H "Content-Type:application/json" -d '{"answer":1}' http://${CHRONOS_HOST}:${CHRONOS_PORT}/api/answer/1  2>&1 | awk '{ if ($1 == "HTTP/1.1") { print $2 } } END { print }' | sed ':a;N;$!ba;s/\n/ /g' | while read httpcode data; do
+      assertEquals "Answer, http code" 200 $httpcode
+      assertEquals "are_u_right " true $(echo "$data" | json -C are_u_right)
+      assertEquals "good_answer " 1 $(echo "$data" | json -C good_answer)
+      assertEquals "Score " 1 $(echo "$data" | json -C score)
+    done
+  done
+done
 
