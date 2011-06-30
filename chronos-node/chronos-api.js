00c6eb51967cef9a2c5cea4d39f337e293d7b7c8
Olivier Bazoud <olivier.bazoud@gmail.com>
2011-06-30 22:00:00 +0200

    Add twitter api

--- chronos-node/chronos-api.js
+++ chronos-node/chronos-api.js
@@ -154,10 +154,11 @@ exports.tweetHttp = function(req, res) {
       res.send(200, {}, 'updateStatus ok:' + data.id);
     }
   });
+};
 
 tweet = function(message, options) {
   var twit = new twitter({
-    consumer_key: 'HxIDFfy6inw58Yk3phxCD',
+    consumer_key: 'HxIDFfy6inw58Yk3phxCDQ',
     consumer_secret: 'uyilyhnaoa5CYKgp8GigXNUTwkvFvZplKA3PVzKs24',
     access_token_key: '274449131-IrvRyqqID58xvkWtmHRn7JvqKwBn3VdK7zmnYoCX',
     access_token_secret: '908pZpyKAa36C3sgd8Y9f2IRTjG0VSKlUXwhhMV7E'
@@ -170,20 +171,18 @@ tweet = function(message, options) {
       }
     }
   })
-  .updateStatus(message,
-    function (data) {
-      if (!(data && data.screen_name == 'usi2011_chronos')) {
-        if (options.error) {
-          options.error(data);
-          return;
-        }
-      }
-      if (options.success) {
-        options.success(data);
+  .updateStatus(message, function (data) {
+    if (!(data && data.screen_name == 'usi2011_chronos')) {
+      if (options.error) {
+        options.error(data);
         return;
       }
     }
-  );
+    if (options.success) {
+      options.success(data);
+      return;
+    }
+  });
 };
 
 exports.getRanking = function(req, res) {
